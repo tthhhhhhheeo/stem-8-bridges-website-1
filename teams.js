f5873e40a8d61a0d7d05d2cc87b90c06cb3fabeb
temp-stem-mr-chin-student <github.garter086@passmail.net>
2024-12-09 10:26:45 -0800

    Added index page

---
+++ teams.js
@@ -0,0 +1,20 @@
+
+var sites = [{
+    "name1": "Coral",
+    "name2": "Ruby",
+    "folder": "Coral, Ruby",
+    "index": "Bridge Attempt1.html"
+},{
+    "name1": "Oban",
+    "name2": "Mason",
+    "folder": "oban mason"
+},{
+    "name1": "Rhys",
+    "name2": "Alekhya",
+    "folder": "rhys and alekhya",
+    "index": "bridge-1.html"
+},{
+    "name1": "Theo",
+    "name2": "Tavish",
+    "folder": "theo and tavish"
+}];
